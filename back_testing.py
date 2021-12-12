07108e5db3666607e1bc3258ad2fa76862d84575
James Moody <James-moody@outlook.com>
2021-12-12 19:59:55 +0000

    First graphical plot added

--- back_testing.py
+++ back_testing.py
@@ -1,13 +1,23 @@
 import yfinance as yahooFinance 
 import datetime
 import pandas as pd
+import mplfinance as mpf
+# from mpl_finance import candlestick2_ohlc
 
+class StrategyTest:
+    def __init__(self):
+        # startDate, endDate, as per our convenience we can modify
+        self.startDate = datetime.datetime(2021, 12, 1) 
+        self.endDate = datetime.datetime(2021, 12, 12)
+        self.candleinterval = "15m"
 
-# startDate , as per our convenience we can modify
-startDate = datetime.datetime(2021, 12, 1) 
-# endDate , as per our convenience we can modify
-endDate = datetime.datetime(2021, 12, 12)
+    def get_data(self):
+        btc_usd = yahooFinance.Ticker("BTC-USD")
+        return btc_usd.history(interval=self.candleinterval, start=self.startDate, end=self.endDate)
 
-btc_usd = yahooFinance.Ticker("BTC-USD")
-
-print(type(btc_usd.history(period="6mo",start=startDate, end=endDate)))
+    def plot_data(self, data):
+        mpf.plot(data, type='candle', mav=(50, 100, 200), volume=True)
+        
+datatest = StrategyTest()
+data = datatest.get_data()
+datatest.plot_data(data)
